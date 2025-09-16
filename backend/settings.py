718e305c8debd53c6f255b061fda1643afb822ea
paraj13 <paraj@coderscotch.com>
2025-09-17 00:17:13 +0530

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -35,6 +35,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
